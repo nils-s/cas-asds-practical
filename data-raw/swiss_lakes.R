3f8e9c6a86294db1313d2eca5cb7445f0ccda8c4
nils-s <28589333+nils-s@users.noreply.github.com>
2024-06-03 19:36:32 +0200

    data: add raw map data
    - add map data for Swiss cantons
    - add map data for Swiss lakes
    - add R scripts to store data as .rda files for package
    data source is the BfS, see links in .R files

---
+++ data-raw/swiss_lakes.R
@@ -0,0 +1,10 @@
+# data source:
+# https://www.bfs.admin.ch/bfs/de/home/dienstleistungen/geostat/geodaten-bundesstatistik/administrative-grenzen/generalisierte-gemeindegrenzen.assetdetail.30487000.html
+
+library(sf)
+
+# detail level G2, Seen (i.e. lakes), data for 2024
+# the shp file requires the corresponding .cpg, .dbf, .prj, and .shx files, so those need to be in the same location
+swiss_lakes <- read_sf("data-raw/lakes/g2s24.shp")
+
+usethis::use_data(swiss_lakes, overwrite = TRUE, compress = "xz")
