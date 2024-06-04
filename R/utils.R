f5fd973a640a355f107f3ebf9b9e061b9a698345
nils-s <28589333+nils-s@users.noreply.github.com>
2024-06-04 20:45:12 +0200

    feat: add R package files
    - DESCRIPTION file defining the package
    - NAMESPACE file defining functions provided by the package
    - some util functions
    - roxygen-generated documentation for data and exported functions
    - README files (.Rmd format, the rendered markdown version, and the
      bibliography file)

---
+++ R/utils.R
@@ -0,0 +1,116 @@
+#' Visually Evaluate Model
+#'
+#' Plot values (in black) and their corresponding fitted values (in red),
+#' according to `model`.
+#'
+#' @param model A fitted model for which to plot its data vs its fitted values. Needs to be [broom::augment()]-able.
+#' @param ... Params passed to [ggplot2::aes()]. Should be the variables to plot, e.g. something like `x = var1, y = var2`.
+#'
+#' @return
+#' A [ggplot2::ggplot()] with two layers (points jittered in x-direction,
+#' representing original values (in black) and fitted values (in red))
+#'
+#' @export
+#'
+#' @examples
+#' plot_data_vs_fitted(lm(mpg ~ disp, data = mtcars), x = disp, y = mpg)
+#' plot_data_vs_fitted(lm(mpg ~ disp + cyl + hp + wt, data = mtcars), x = wt, y = mpg)
+plot_data_vs_fitted <- function(model, ...) {
+  jitter_width <- 0.25
+  jitter_height <- 0
+  model |>
+    broom::augment() |>
+    ggplot2::ggplot(ggplot2::aes(...)) +
+    ggplot2::geom_jitter(width = jitter_width, height = jitter_height) +
+    ggplot2::geom_jitter(ggplot2::aes(y = .fitted), color = "red", width = jitter_width, height = jitter_height)
+}
+
+#' Create Linestrings from Points
+#'
+#' Converts a collection of points to a collection of linestrings, for faster plotting.
+#' Points are grouped by `date`, and are assumed to be ordered.
+#' Note that input data will be [dplyr::summarize()]'d by `date`, i.e. output will probably be
+#' much shorter than input. Additionally, summarizing gets rid of all other information, i.e.
+#' return value only contains dates and linestrings and will have to be joined with any additional
+#' desired information.
+#'
+#' @param points A data.frame or tibble with (at least) a `date` and an sf geometry column containing a point for each observation
+#'
+#' @return A tibble with two columns: `date`, and an sf geometry column, containing a linestring for each observation
+#'
+#' @export
+#'
+#' @examples
+#' library(sf)
+#' library(dplyr)
+#' library(ggplot2)
+#' points <- track_details |>
+#'   filter(!is.na(latitude) & !is.na(longitude)) |>
+#'   st_as_sf(coords = c("longitude", "latitude"), crs = "WGS84")
+#' to_linestrings(points) |>
+#'   ggplot() +
+#'   geom_sf()
+to_linestrings <- function(points) {
+  points |>
+    dplyr::group_by(date) |>
+    dplyr::summarize(do_union = FALSE) |> # `do_union = FALSE` -> see https://github.com/r-spatial/sf/issues/321#issuecomment-342065386
+    sf::st_cast("LINESTRING")
+}
+
+#' Summarize a Collection of Detailed Track Points Grouped by Date
+#'
+#' @param tracks A data.frame or tibble of track detail data points, e.g. [track_details]
+#'
+#' @return
+#' A tibble summarizing `tracks`, one row per date. Values (i.e. columns) are:
+#' - `date` the date of the trip
+#' - `weekday` weekday corresponding to `date`; factor with English weekday names; computed from `date`, only included for convenience
+#' - `distance_km` length of the track (in kilometers)
+#' - `time_min` duration of the track (in minutes)
+#' - `altitude_gain_m` total altitude gained during the track (in meters), without compensating for altitude lost
+#' - `temperature_c` median temperature of the track (in degrees Celsius)
+#' - `speed_km_h` average speed of the track (in km/h)
+#' - `avg_hr_bpm` average heart rate for the track (in beats per minute)
+#' - `below_zones_min` time spent below heart rate zone 1 (i.e. < 60% of max. hr / < 109 bpm) (in minutes)
+#' - `zone1_min` time spent in heart rate zone 1 (i.e. [60;70)% of max. hr / [109;127) bpm) (in minutes)
+#' - `zone2_min` time spent in heart rate zone 2 (i.e. [70;80)% of max. hr / [127;145) bpm) (in minutes)
+#' - `zone3_min` time spent in heart rate zone 3 (i.e. [80;90)% of max. hr / [145;164) bpm) (in minutes)
+#' - `zone4_min` time spent in heart rate zone 4 (i.e. [90;100)% of max. hr / [164;182) bpm) (in minutes)
+#' - `above_zones_min` time spent above heart rate zone 4 (i.e. >= 100% of max. hr / 182 bpm) (in minutes)
+#' - `samples` number of data point for the track, i.e. the number of data points which were aggregated for this row
+#'
+#' @export
+#'
+#' @examples
+#' # summarize all detailed data points by date
+#' summarize_tracks(track_details)
+#' # summarize only a specific date
+#' library(dplyr)
+#' track_details |>
+#'   filter(date == "2020-06-01") |>
+#'   summarize_tracks()
+summarize_tracks <- function(tracks) {
+  tracks |>
+    dplyr::group_by(date) |>
+    dplyr::mutate(uphill_m = cumsum(altitude_differences_uphill_m)) |>
+    dplyr::summarize(
+      distance_km = max(distance_absolute_m) / 1000,
+      time_min = max(training_time_absolute_s) / (100 * 60),
+      altitude_gain_m = max(uphill_m),
+      temperature_c = stats::median(temperature_c),
+      speed_km_h = mean(speed_m_s) * 3.6,
+      avg_hr_bpm = mean(heartrate_bpm),
+      below_zones_min = max(cumsum(time_below_intensity_zones_s)) / (100 * 60),
+      zone1_min = max(cumsum(time_in_intensity_zone1_s)) / (100 * 60),
+      zone2_min = max(cumsum(time_in_intensity_zone2_s)) / (100 * 60),
+      zone3_min = max(cumsum(time_in_intensity_zone3_s)) / (100 * 60),
+      zone4_min = max(cumsum(time_in_intensity_zone4_s)) / (100 * 60),
+      above_zones_min = max(cumsum(time_above_intensity_zones_s)) / (100 * 60),
+      samples = dplyr::n()
+    ) |>
+    dplyr::mutate(
+      # temporarily use en_US locale, so weekdays() gives English day names regardless of user's locale
+      # note: this is for Linux, the locale string might be different on other OSs
+      weekday = forcats::fct(withr::with_locale(c("LC_TIME" = "en_US.UTF-8"), weekdays(date)))
+    )
+}
---
+++ README.md
@@ -0,0 +1,159 @@
+
+<!-- README.md is generated from README.Rmd. Please edit that file -->
+
+# asds2024.nils.practical
+
+<!-- badges: start -->
+<!-- badges: end -->
+
+This module contains the final practical part for the Certificate of
+Advanced Studies in Advanced Statistical Data Science ([CAS
+ASDS](https://www.unibe.ch/weiterbildungsangebote/cas_advanced_statistical_data_science/index_ger.html))
+at the [University of Berne](https://www.unibe.ch/index_eng.html) for
+the class of 2024.
+
+## Format
+
+The practical module has been packaged as an R package, so everything
+(data, scripts, reports, …) should be contained in a single bundle, and
+analyses should be reproducible.
+
+This setup follows suggestions from (Marwick, Boettiger, and Mullen
+2018b, 2018a), (Flight 2014), and (Wickham and Bryan 2023) (which
+provided more or less the instructions and toolchain recommendations
+based on which this package has been created).
+
+There are other opinions and tools (e.g. (Flight 2021) and (Landau 2024,
+2021)) for a lighter-weight reproducible research approach, which I
+might explore in the future.
+
+For more inspiration and available tools, see (Blischak et al. 2024).
+
+## Installation
+
+You can install the development version of `asds2024.nils.practical`
+from [GitHub](https://github.com/) with:
+
+``` r
+# install.packages("devtools") # <- for `install_github` to be available uncomment this and run it (unless you've already installed it)
+
+# Notes:
+# - you probably want to install the suggested dependencies as well, since this package only uses suggested dependencies
+# - when `install_github`-ing, you need to explicitly specify that you want the vignettes built as well
+devtools::install_github(
+  "nils-s/cas-asds-practical",
+  dependencies = c("Depends", "Imports", "LinkingTo", "Suggests"),
+  build_vignettes = TRUE)
+```
+
+Since not all documents are provided as vignettes, you probably want to
+clone the package sources into a local directory as well:
+
+``` bash
+git clone https://github.com/nils-s/cas-asds-practical.git
+```
+
+From there, you can more directly explore the raw data, and read
+documents that are not packaged as vignettes.
+
+### Installation Troubleshooting
+
+Assuming the `devtools` package is installed (and `install_github` is
+available), this package by itself should not cause problems (simply
+because it contains very little stuff that could cause problems).
+However, it depends on a bunch of dependencies, which will be installed
+when installing this package’s suggested dependencies as shown in the
+code snippet above.
+
+The main suspect in this regard is the `sf` package, which has a few
+dependencies of its own (not all of which are R packages). The first
+thing to try (after studying the error messages, of course) is to make
+sure all prerequisites for `sf` are fulfilled (e.g. the
+[GEOS](https://libgeos.org), [GDAL](https://gdal.org), and
+[PROJ](https://proj.org/) libraries).
+
+On a Fedora machine, the following should get you started:
+
+``` bash
+sudo dnf install gdal gdal-devel udunits2-devel proj proj-devel geos geos-devel
+```
+
+See [the `sf` documentation](https://r-spatial.github.io/sf/) for more
+information.
+
+## Example
+
+``` r
+library(asds2024.nils.practical)
+vignette("get-started", package = "asds2024.nils.practical")
+```
+
+## References
+
+<div id="refs" class="references csl-bib-body hanging-indent">
+
+<div id="ref-cran-view-rr" class="csl-entry">
+
+Blischak, John, Alison Hill, Ben Marwick, Daniel Sjoberg, and Will
+Landau. 2024. “CRAN Task View: Reproducible Research.” February 20,
+2024. <https://cran.r-project.org/view=ReproducibleResearch>.
+
+</div>
+
+<div id="ref-mflight2014" class="csl-entry">
+
+Flight, Robert M. 2014. “Analyses as Packages.” July 28, 2014.
+<https://rmflight.github.io/posts/2014-07-28-analyses-as-packages>.
+
+</div>
+
+<div id="ref-mflight2021" class="csl-entry">
+
+———. 2021. “Packages Don’t Work Well for Analyses in Practice.” March 2,
+2021.
+<https://rmflight.github.io/posts/2021-03-02-packages-dont-work-well-for-analyses-in-practice>.
+
+</div>
+
+<div id="ref-targets2021" class="csl-entry">
+
+Landau, William Michael. 2021. “The <span class="nocase">targets</span>
+R Package: A Dynamic Make-Like Function-Oriented Pipeline Toolkit for
+Reproducibility and High-Performance Computing.” *Journal of Open Source
+Software* 6 (57): 2959. <https://doi.org/10.21105/joss.02959>.
+
+</div>
+
+<div id="ref-R-targets" class="csl-entry">
+
+———. 2024. *<span class="nocase">targets</span>: Dynamic
+Function-Oriented Make-Like Declarative Pipelines*.
+<https://docs.ropensci.org/targets/>.
+
+</div>
+
+<div id="ref-marwick2018-tas" class="csl-entry">
+
+Marwick, Ben, Carl Boettiger, and Lincoln Mullen. 2018a. “Packaging Data
+Analytical Work Reproducibly Using R (and Friends).” *The American
+Statistician* 72 (1): 80–88.
+<https://doi.org/10.1080/00031305.2017.1375986>.
+
+</div>
+
+<div id="ref-marwick2018-peerj" class="csl-entry">
+
+———. 2018b. “Packaging Data Analytical Work Reproducibly Using R (and
+Friends).” *PeerJ Preprints* 6 (March): e3192v2.
+<https://doi.org/10.7287/peerj.preprints.3192v2>.
+
+</div>
+
+<div id="ref-rpackages2e" class="csl-entry">
+
+Wickham, Hadley, and Jennifer Bryan. 2023. *R Packages*. 2. ed.
+O’Reilly. <https://r-pkgs.org>.
+
+</div>
+
+</div>
